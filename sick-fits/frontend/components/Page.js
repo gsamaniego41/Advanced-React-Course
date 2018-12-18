f05e3bc7cb712115481dd48af0b3f2de4dbece57
Gabriel Samaniego <selfdev.gs@gmail.com>
2018-12-17 17:51:19 -0800

    initial Header and Nav Components

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,7 +1,12 @@
 import React, {Component} from "react";
-
+import Header from "./Header";
 export default class Page extends Component {
   render() {
-    return <div>{this.props.children}</div>;
+    return (
+      <div>
+        <Header />
+        {this.props.children}
+      </div>
+    );
   }
 }
