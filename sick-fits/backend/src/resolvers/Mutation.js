5d3d3ccb7362ac85a04cf35d9af1b99d7dcae8b3
Gabriel Samaniego <selfdev.gs@gmail.com>
2018-12-21 12:03:31 -0800

    first query and mutation, items creation

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,24 @@
-const mutations = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    // TODO: Check if they are logged in
+    const item = await ctx.db.mutation.createItem(
+      // returns a Promise
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
+    return item;
+  }
 
-module.exports = mutations;
+  // createDog(parent, args, ctx, info) {
+  //   global.dogs = global.dogs || [];
+  //   const newDog = {name: args.name};
+  //   global.dogs.push(newDog);
+  //   return newDog;
+  // }
+};
+
+module.exports = Mutations;
